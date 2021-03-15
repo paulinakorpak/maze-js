39851c84157f4dd8a9e2a237857aedc8e684f430
Paulina Korpak <korpak.paulina@gmail.com>
2021-03-15 20:13:23 +0000

    Add start animation

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { Game } from './Game';
 const appElement = document.querySelector('.app');
 const fieldSize = 20;
 
-Game(appElement, fieldSize).init();
+setTimeout(() => Game(appElement, fieldSize).init(), 2000);
